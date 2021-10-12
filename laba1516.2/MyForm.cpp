8d6f6c02bb6c2d5e542afc6d1d6150e6cdfe9c89
DevilShark <DevilShark@TOFU>
2021-10-12 16:18:40 +0300

    Добавьте файлы проекта.

---
+++ laba1516.2/MyForm.cpp
@@ -0,0 +1,11 @@
+#include "MyForm.h"
+#include <Windows.h>
+
+using namespace laba15162;
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    Application::Run(gcnew MyForm);
+    return 0;
+}
